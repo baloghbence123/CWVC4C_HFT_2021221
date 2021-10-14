cf7a4d20b28e0b74f54305dcee6cf7808d623cf9
yxtasy <baloghbence123@gmail.com>
2021-10-14 14:26:35 +0200

    element class created

---
+++ CWVC4C_HFT_2021221.Models/Element.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CWVC4C_HFT_2021221.Models
+{
+    public class Element
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ElementId { get; set; }
+
+        public string Name { get; set; }
+
+
+        [NotMapped]
+        public virtual ICollection<Hero> Heroes { get; set; }
+
+        public Element()
+        {
+            Heroes = new HashSet<Hero>();
+        }
+
+    }
+}
