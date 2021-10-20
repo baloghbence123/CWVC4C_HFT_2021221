5b6ea10737cbbf933fc980f87c48523062fa47fa
yxtasy <baloghbence123@gmail.com>
2021-10-20 20:23:05 +0200

    Element logic created with IElementLogic

---
+++ CWVC4C_HFT_2021221.Logic/ElementLogic.cs
@@ -0,0 +1,54 @@
+﻿using CWVC4C_HFT_2021221.Models;
+using CWVC4C_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CWVC4C_HFT_2021221.Logic
+{
+    public class ElementLogic : IElementLogic
+    {
+
+        IElementRepository elementRepository;
+        public ElementLogic(IElementRepository elementRepository)
+        {
+            this.elementRepository = elementRepository;
+        }
+        public void Create(Element element)
+        {
+            if (element.Name != null)
+            {
+                elementRepository.Create(element);
+            }
+            else throw new ArgumentException("Name is required");
+            
+        }
+        public Element Read(int id)
+        {
+            return elementRepository.Read(id);
+
+        }
+        public IEnumerable<Element> ReadAll()
+        {
+            return elementRepository.ReadAll();
+        }
+        public void Delete(int id)
+        {
+            elementRepository.Delete(id);
+        }
+        public void Update(Element element)
+        {
+            if (element.Name!=null)
+            {
+                elementRepository.Update(element);
+            }
+            else throw new ArgumentException("Name is required");
+
+        }
+
+
+
+    }
+}
---
+++ CWVC4C_HFT_2021221.Logic/IElementLogic.cs
@@ -0,0 +1,14 @@
+﻿using CWVC4C_HFT_2021221.Models;
+using System.Collections.Generic;
+
+namespace CWVC4C_HFT_2021221.Logic
+{
+    interface IElementLogic
+    {
+        void Create(Element hero);
+        void Delete(int id);
+        Element Read(int id);
+        IEnumerable<Element> ReadAll();
+        void Update(Element hero);
+    }
+}
