c100121a90f80890e634ea01287784754c4488ea
yxtasy <baloghbence123@gmail.com>
2021-10-20 20:25:29 +0200

    Created the basics, ready To test

--- CWVC4C_HFT_2021221.Client/Program.cs
+++ CWVC4C_HFT_2021221.Client/Program.cs
@@ -1,5 +1,9 @@
 ﻿using CWVC4C_HFT_2021221.Data;
+using CWVC4C_HFT_2021221.Logic;
+using CWVC4C_HFT_2021221.Repository;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CWVC4C_HFT_2021221.Client
 {
@@ -9,8 +13,17 @@ namespace CWVC4C_HFT_2021221.Client
         {
 
             HeroDbContext db = new HeroDbContext();
+
+            AbilityLogic a1 = new AbilityLogic(new AbilityRepository(db));
+            HeroLogic h1 = new HeroLogic(new HeroRepository(db));
+            ElementLogic e1 = new ElementLogic(new ElementRepository(db));
             ;
+
+
+                     
+
             
+
         }
     }
 }
