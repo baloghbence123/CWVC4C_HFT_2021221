fccf601c00f756a80a680a672db231f08cfbf65b
yxtasy <baloghbence123@gmail.com>
2021-10-20 17:55:52 +0200

    created IElementRepository interface

---
+++ CWVC4C_HFT_2021221.Repository/IElementRepository.cs
@@ -0,0 +1,14 @@
+﻿using CWVC4C_HFT_2021221.Models;
+using System.Linq;
+
+namespace CWVC4C_HFT_2021221.Repository
+{
+    public interface IElementRepository
+    {
+        void Create(Element element);
+        void Delete(int id);
+        Element Read(int id);
+        IQueryable<Element> ReadAll();
+        void Update(Element element);
+    }
+}
