ae184d8da0b03260b99ddaea609990471f75b230
yxtasy <baloghbence123@gmail.com>
2021-10-20 17:54:15 +0200

    Created IAbility repository

---
+++ CWVC4C_HFT_2021221.Repository/IAbilityRepository.cs
@@ -0,0 +1,14 @@
+﻿using CWVC4C_HFT_2021221.Models;
+using System.Linq;
+
+namespace CWVC4C_HFT_2021221.Repository
+{
+    public interface IAbilityRepository
+    {
+        void Create(Ability ability);
+        void Delete(int id);
+        Ability Read(int id);
+        IQueryable<Ability> ReadAll();
+        void Update(Ability ability);
+    }
+}
