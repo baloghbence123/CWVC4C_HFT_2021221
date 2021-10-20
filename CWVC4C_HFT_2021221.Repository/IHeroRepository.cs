cf90f7af489f424acddfbfd1c65a8121cbd1b81b
yxtasy <baloghbence123@gmail.com>
2021-10-20 17:55:26 +0200

    Created IHeroRepository interface

---
+++ CWVC4C_HFT_2021221.Repository/IHeroRepository.cs
@@ -0,0 +1,14 @@
+﻿using CWVC4C_HFT_2021221.Models;
+using System.Linq;
+
+namespace CWVC4C_HFT_2021221.Repository
+{
+    public interface IHeroRepository
+    {
+        void Create(Hero hero);
+        void Delete(int id);
+        Hero Read(int id);
+        IQueryable<Hero> ReadAll();
+        void Update(Hero hero);
+    }
+}
